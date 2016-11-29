205e323a4fc2f58c14c20f8844facc8ed8a4cbf2
Sam Pellegrene <pellegrenesam@gmail.com>
2016-11-29 11:03:02 -0600

    added load and media query

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+
+console.log("Welcome to the console! I hope you are enjoying my webpage. I am always open to new opportunities and although I have much to learn, I am highly motivated to do whatever I can to help serve your team best.")
